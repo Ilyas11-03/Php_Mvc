d68fe0b650b4886cf30513df4a31d0082ba4a2e0
Ilyas11-03 <ilyas.aboulkassim@gmail.com>
2025-09-18 12:18:57 +0100

    secondcommit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once 'controller/stagiaire_controller.php'; // Include the controller fi
 listeStagiaireAction(); // Call the list action to display all interns
 
 
-$conn = new mysqli("localhost", "root", "", "stagiare");
+$conn = new mysqli("localhost", "root", "", "ofppt");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
